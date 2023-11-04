e57eef74078a926ce99ed9ec00cada9649a5422f
Rishith Gandham <rishith.gandham@gmail.com>
2023-11-04 13:24:17 -0400

    api key hide

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 console.log(process.env.FIREBASE_API_KEY);
 const firebaseConfig = {
-  apiKey: "AIzaSyAn4-Qm6SWtVWnmRTiXcuvmkYyGDWQ3psc",
-  authDomain: "volunteer-app-218ac.firebaseapp.com",
-  projectId: "volunteer-app-218ac",
-  storageBucket: "volunteer-app-218ac.appspot.com",
-  messagingSenderId: "1067725673313",
-  appId: "1:1067725673313:web:cda283587cbca4b271eb20",
-  measurementId: "G-2TZHJDZNCY"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
